3760fda9cd271b659129d59b8d4c184fc3e110b4
Orel star <galban81@gmail.com>
2018-10-09 23:17:05 +0300

    sdf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,10 +14,6 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
